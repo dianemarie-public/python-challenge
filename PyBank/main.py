0618fa0563b32acf2b55d1cf125b60c7e783e19e
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2020-09-20 15:44:29 -0500

    Updates main.py for cleanup of code and comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,37 +10,37 @@
 
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
-# Import dependencies for creating file paths across operating systems and reading csv files Ref: imports.py
-import os
-import csv
+import os # Imports the dependency for creating file paths across operating systems
+import csv # Imports the dependency for reading csv files Ref: imports.py
 
-# Set the file path using the os module (imported above), and print
-csvfile = os.path.join('Resources', 'budget_data.csv')
-print(f"CSV File: {csvfile}")
-
+budget_file = os.path.join('Resources', 'budget_data.csv') # Set the file path using the os module (imported above)
 output_path = os.path.join("Analysis", "output.txt")
-print(f"Analysis File: {output_path}")
 
-# Open and read csv file using the csv module, (imported above) Ref: examples read_csv.py, cereal_solved.py
-with open(csvfile) as csvfile:
-    # Using DictReader from the csv module, creates an object that maps each row to a dictionary where keys are given fieldnames, defaulted to the first header row (ref: Python Documentation for csv module here https://docs.python.org/3/library/csv.html#module-contents)
-    dict_reader = csv.DictReader(csvfile)    
-    # Prints field names (i.e. header row)
-    print(f"Field Names: {dict_reader.fieldnames}")
-    # Read and print each row as dictionary
-    for rows in dict_reader:
-        next(dict_reader)
-        print(rows)
-    # Print the number of rows, minus the header row
-    budget_months = dict_reader.line_num - 1
-    print(f"Number of Budget Months: {budget_months}")
+with open(budget_file, 'r') as csvfile: # Opens and reads the file using the csv module (imported above) Ref: examples read_csv.py, cereal_solved.py
+
+    budget_reader = csv.DictReader(csvfile) # Initiates DictReader from the csv module to create an object that maps each row to a dictionary, where keys are given fieldnames, defaulted to the first header row (ref: Python Documentation for csv module here https://docs.python.org/3/library/csv.html#module-contents)
+
+    for rows in budget_reader: # Reads each row of the dictionary
+        next(budget_reader) # Reads the next row
+        
+        # print(sum(net_budget.values())) # Ref: https://stackoverflow.com/questions/4880960/how-to-sum-all-the-values-in-a-dictionary
+    net_budget = 0
+    budget_months = budget_reader.line_num - 1 # Creates variable for counting the number of lines, minus the header row  
+
+print(f"Budget File: {budget_file}") # Prints read file name
+print(f"Analysis File: {output_path}") # Prints write file name
+print(f"Field Names: {budget_reader.fieldnames}") # Prints field names (i.e. header row)
+print(f"Number of Budget Months: {budget_months}") # Prints the variable value
+print(f"Net Profit/Losses: {net_budget}") # Prints the variable value
+
+# Open output file and write to it (ref: Python Documentation for csv.writer here https://docs.python.org/3/library/csv.html)
 with open(output_path, "w", newline='') as csvfile:
     outputwriter = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-    outputwriter.writerow(['Number of budget months: '])
-    outputwriter.writerow(['Net profit/losses: '])
+    outputwriter.writerow(['Number of budget months: ', budget_months]) # WORKS
+        ############ WORKS ABOVE THIS LINE ########
+    outputwriter.writerow(['Net profit/losses: ', net_budget])
     outputwriter.writerow(['Average profit/losses: '])
     outputwriter.writerow(['Greatest increase profit/losses: '])
     outputwriter.writerow(['Greatest decrease profit/losses: '])
     outputwriter.writerow(['Net profit/losses: '])
 
-    ############ WORKS ABOVE THIS LINE ########
