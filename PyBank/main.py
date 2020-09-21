4a61dba37371c76cd8b20c4a3c5b6a6902af99d0
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2020-09-21 01:29:07 -0500

    Updates main.py  with working code
    Includes all comments and references to web sources  used  to solve for:
    converting  strings to integers,
    loop through rows to find largest and smallest value
    format variables before output with comma separators, dollar signs and two decimal points
    eliminating blank lines in output file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,12 +2,6 @@
 
 # Create a Python script that analyzes the records to calculate each of the following:
 
-    # The total number of months included in the dataset
-    # The net total amount of "Profit/Losses" over the entire period
-    # The average of the changes in "Profit/Losses" over the entire period
-    # The greatest increase in profits (date and amount) over the entire period
-    # The greatest decrease in losses (date and amount) over the entire period
-
 import os 
 import csv 
 # Imports the dependencies to: create file paths across operating systems (os) and read from csv files (csv)
@@ -15,40 +9,48 @@ import csv
 csv_path = os.path.join('Resources', 'budget_data.csv') 
 text_path = os.path.join("Analysis", "output.txt") 
 # Set the csv (read from) and text (write to) file paths using the os module (imported above)
-
-# Creates variables, and set initial vale to zero (integers)
-num_months = 0
-net_profit_loss = 0
-avg_profit_loss = 0
-max_profit = 0
-max_loss = 0
+file_name = 'budget_data.csv'
+# Creates variables, and set initial values to zero (as integers)
+num_months = 0 # The total number of months included in the dataset
+net_profit_loss = 0 # The net total amount of "Profit/Losses" over the entire period
+avg_profit_loss = 0 # The average of the changes in "Profit/Losses" over the entire period
+max_profit = 0 # The greatest increase in profits (date and amount) over the entire period
+max_loss = 0 # The greatest decrease in losses (date and amount) over the entire period
 
 with open(csv_path, 'r') as csvfile: # Opens and reads the file using the csv module
-    csv_reader = csv.DictReader(csvfile) # Initiates DictReader from csv module
-    print(f"CSV Reader: {csv_reader}")
-    
+    csv_reader = csv.DictReader(csvfile) # Initiates DictReader from csv module, and the ability to reference dictionary keys (i.e. "Profit/Losses")
     for row in csv_reader: # Reads each row of the dictionary
-       budget_row = int(row["Profit/Losses"]) # Converts content of column to integer, example here: https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html  
-       
-       if budget_line in profit_loss:           
-           budget_line = [int(i) for i in row[1:]] 
-    budget_months = budget_reader.line_num - 1 # Creates variable for counting the number of lines, minus the header row  
+        budget_row = int(row["Profit/Losses"]) # Converts content of column to integer, example here: https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html
+        net_profit_loss += int(row["Profit/Losses"])
+        if max_profit == 0 or max_profit < budget_row: # If statement example for largest value found here: https://courses.cs.washington.edu/courses/cse140/13wi/csv-parsing.html
+            max_profit = int(budget_row)
+        if max_loss == 0 or max_loss > budget_row: # If statement example (above) reversed for smallest value
+            max_loss = int(budget_row)   
+    num_months = csv_reader.line_num - 1 # Creates variable for counting the number of lines, minus the header row     
+    avg_profit_loss = "${:,.2f}".format(net_profit_loss/num_months) # Formats the variable with two decimal points, comma separators and a preceeding dollar sign
+    net_profit_loss = "${:,.2f}".format(net_profit_loss) # Formats the variable with two decimal points, comma separators and a preceeding dollar sign
+    max_profit = "${:,.2f}".format(max_profit) # Formats the variable with two decimal points, comma separators and a preceeding dollar sign
+    max_loss = "${:,.2f}".format(max_loss) # Formats the variable with two decimal points, comma separators and a preceeding dollar sign
+    # Formatting with comma separators: https://www.kite.com/python/answers/how-to-add-commas-to-a-number-in-python
+    # Formatting with two decimal points: https://www.kite.com/python/answers/how-to-print-a-float-with-two-decimal-places-in-python#:~:text=Use%20str.,float%20with%20two%20decimal%20places&text=format(number)%20with%20%22%7B,string%20to%20print%20the%20float.
+    # Formatting with leading dollar sign:  https://www.daniweb.com/programming/software-development/threads/463071/percentages-and-dollar-signs-in-output 
+    
+    print(f"CSV Path: {csv_path}") # Prints to terminal
+    print(f"Analysis Path: {text_path}") # Prints to terminal
+    print(f" ") # Prints to terminal
+    print(f"Budget Analysis of {file_name}") # Prints to terminal
+    print(f"Number of Months: {num_months}") # Prints to terminal
+    print(f"Net Profit/Loss: {net_profit_loss}") # Prints to terminal
+    print(f"Average Profit/Loss: {avg_profit_loss}") # Prints to terminal
+    print(f"Greatest Increase Profit/Loss: {max_profit}") # Prints to terminal
+    print(f"Greatest Decrease Profit/Loss: {max_loss}") # Prints to terminal
 
-# Prints analysis to terminal
-print(f"Budget Analysis")
-print(f"Number of Budget Months: {budget_months}") 
-print(f"Last Budget Line Converted to Integers: {budget_line}")
-print(f"Net Profit/Losses: {net_budget}") 
-print(f"Average Profit/Losses: {avg_budget}") 
-print(f"Greatest Increase Profit/Losses: {max_profit}") 
-print(f"Greatest Decrease Profit/Losses: {max_loss}") 
-print(f"Field Names: {budget_reader.fieldnames}") 
-
-# Prints analysis to the output.txt file
-with open(output_path, "w", newline='') as csvfile: # Opens output file, and writes to it (ref: Python Documentation here https://docs.python.org/3/library/csv.html#writer-objects
-    outputwriter = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-    outputwriter.writerow(['Number of budget months: ', budget_months]) # WORKS
-    outputwriter.writerow(['Net profit/losses: ', net_budget])
-    outputwriter.writerow(['Average profit/losses: ', avg_budget])
-    outputwriter.writerow(['Greatest increase profit/losses: ', max_profit])
-    outputwriter.writerow(['Greatest decrease profit/losses: ', max_loss])
+# Opens output file, and writes to it (ref: Python Documentation here https://docs.python.org/3/library/csv.html#writer-objects
+with open(text_path, "w", newline='') as csvfile: # Eliminate blank lines in output: https://stackoverflow.com/questions/3348460/csv-file-written-with-python-has-blank-lines-between-each-row
+    text_writer = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL) # Used example included in csv.writer (i.e. spamwriter): https://docs.python.org/3/library/csv.html
+    text_writer.writerow(['Budget Analysis of',file_name]) # Prints to the output.txt file
+    text_writer.writerow(['Number of Months:',num_months]) # Prints to the output.txt file
+    text_writer.writerow(['Net Profit/Loss:',net_profit_loss]) # Prints to the output.txt file
+    text_writer.writerow(['Average Profit/Losses:',avg_profit_loss]) # Prints to the output.txt file
+    text_writer.writerow(['Greatest Increase Profit/Loss:',max_profit]) # Prints to the output.txt file
+    text_writer.writerow(['Greatest Decrease Profit/Loss:',max_loss]) # Prints to the output.txt file
