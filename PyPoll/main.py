9f4f216a3398e297807b19663ab15d6e3a7a2455
dianemarie-public <69361953+dianemarie-public@users.noreply.github.com>
2020-09-22 17:19:46 -0500

    Updates main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,3 @@
-## PyPoll
-
-# In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
-
-# You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. 
-
-# Your task is to create a Python script that analyzes the votes and calculates each of the following:
-
-#   * The total number of votes cast
 #   * A complete list of candidates who received votes
 #   * The percentage of votes each candidate won
 #   * The total number of votes each candidate won
@@ -14,7 +5,7 @@
 
 import os
 import csv
-import collections
+import collections 
 
 csv_path = os.path.join('Resources', 'election_data.csv')
 text_path = os.path.join("Analysis", "election_results.txt")
@@ -22,6 +13,7 @@ text_path = os.path.join("Analysis", "election_results.txt")
 csv_file_name = 'election_data.csv'
 txt_file_name = 'election_reslts.txt'
 
+voters = collections.Counter()
 candidate1_votes = 0
 candidate2_votes = 0
 candidate3_votes = 0
@@ -32,37 +24,37 @@ winner = 'none'
 with open(csv_path, 'r') as csvfile:
     csv_reader = csv.DictReader(csvfile)
     for votes in csv_reader:
-        if votes['Candidate'] == 'Khan': # Using DictReader the key is the column name, then searches for 'word'
-            candidate1_votes += 1 # Totals the number of times 'word' is found in column 'Candidate
-    
-        if votes['Candidate'] == 'Correy': # Using DictReader the key is the column name, then searches for 'word'
-            candidate2_votes += 1 # Totals the number of times 'word' is found in column 'Candidate
-    
-        if votes['Candidate'] == 'Li': # Using DictReader the key is the column name, then searches for 'word'
-            candidate3_votes += 1 # Totals the number of times 'word' is found in column 'Candidate
 
-        if votes['Candidate'] == "O'Tooley": # Using DictReader the key is the column name, then searches for 'word'
-            candidate4_votes += 1 # Totals the number of times 'word' is found in column 'Candidate
-        
+        # Counts the number of votes for each candidate                
+        if votes['Candidate'] == 'Khan': # Using DictReader the key is the column name, then searches for 'word'
+            candidate1_votes += 1 # Totals the number of times 'word' is found in column 'Candidate    
+        if votes['Candidate'] == 'Correy':
+            candidate2_votes += 1    
+        if votes['Candidate'] == 'Li': 
+            candidate3_votes += 1
+        if votes['Candidate'] == "O'Tooley":
+            candidate4_votes += 1
+
+        # Compares the number of votes for each candidate
         if candidate1_votes > candidate2_votes and candidate1_votes > candidate3_votes and candidate1_votes > candidate4_votes:
-            winner = "Khan"
-        
+            winner = "Khan"        
         if candidate2_votes > candidate1_votes and candidate2_votes > candidate3_votes and candidate2_votes > candidate4_votes:
             winner = "Correy"
-
         if candidate3_votes > candidate1_votes and candidate3_votes > candidate2_votes and candidate3_votes > candidate4_votes:
             winner = "Li"
-
         if candidate4_votes > candidate1_votes and candidate4_votes > candidate2_votes and candidate4_votes > candidate3_votes:
             winner = "O'Tooley"
 
-    total_votes = candidate1_votes + candidate2_votes + candidate3_votes + candidate4_votes
+    num_lines = csv_reader.line_num - 1
+    total_votes = candidate1_votes + candidate2_votes + candidate3_votes + candidate4_votes # Total votes cast
     candidate1_pct = candidate1_votes/total_votes*100
     candidate2_pct = candidate2_votes/total_votes*100
     candidate3_pct = candidate3_votes/total_votes*100
     candidate4_pct = candidate4_votes/total_votes*100
+    total_pct = candidate1_pct + candidate2_pct + candidate3_pct + candidate4_pct
    
     # Formats variables before printing
+    num_lines = "{:,.0f}".format(num_lines)
     total_votes = "{:,.0f}".format(total_votes)
     candidate1_votes = "{:,.0f}".format(candidate1_votes)
     candidate2_votes = "{:,.0f}".format(candidate2_votes)
@@ -71,26 +63,26 @@ with open(csv_path, 'r') as csvfile:
     candidate1_pct = "{:,.0f}%".format(candidate1_pct)
     candidate2_pct = "{:,.0f}%".format(candidate2_pct)
     candidate3_pct = "{:,.0f}%".format(candidate3_pct)
-    candidate4_pct = "{:,.0f}%".format(candidate4_pct)    
+    candidate4_pct = "{:,.0f}%".format(candidate4_pct)
+    total_pct = "{:,.0f}%".format(total_pct)   
     
     # Prints to terminal
     print(f"Election Results")
-    print(f"Total Votes Cast: {total_votes}")
+    print(f"Total Votes Cast: {total_votes}, {total_pct}")
     print(f"Votes for Khan: {candidate1_votes}, {candidate1_pct}")
     print(f"Votes for Correy: {candidate2_votes}, {candidate2_pct}")
     print(f"Votes for Li: {candidate3_votes}, {candidate3_pct}")
     print(f"Votes for O'Tooley: {candidate4_votes}, {candidate4_pct}")
-    print(f"Candidate With Most Votes: {winner}")
+    print(f"Election Winner: {winner}")    
 
 with open (text_path, "w", newline='') as csvfile:
     text_writer = csv.writer(csvfile, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
 
     # Prints to election_results.txt
     text_writer.writerow(["Election Results"])
-    text_writer.writerow(["Total Votes Cast: ",total_votes])
-    #text_writer.writerow(['Candidates With Votes: ',candidate_votes])
-    text_writer.writerow(["Khan Votes: ",candidate1_votes,candidate1_pct])
-    text_writer.writerow(["Correy Votes: ",candidate2_votes,candidate2_pct])
-    text_writer.writerow(["Li Votes: ",candidate3_votes,candidate3_pct])
-    text_writer.writerow(["O'Tooley Votes: ",candidate4_votes,candidate4_pct])
-    text_writer.writerow(["Election Winner: ",winner])
+    text_writer.writerow(["Total Votes Cast ==",total_votes,"==",total_pct])
+    text_writer.writerow(["Khan Votes =========",candidate1_votes,"==",candidate1_pct])
+    text_writer.writerow(["Correy Votes =======",candidate2_votes,"====",candidate2_pct])
+    text_writer.writerow(["Li Votes ===========",candidate3_votes,"====",candidate3_pct])
+    text_writer.writerow(["O'Tooley Votes =====",candidate4_votes,"====",candidate4_pct])
+    text_writer.writerow(["Election Winner ====",winner])
